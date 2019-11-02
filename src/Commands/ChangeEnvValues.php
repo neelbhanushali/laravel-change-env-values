d9217316a827dbb6cf8dcfd2c0eb3e904fc0f1ca
Neel Bhanushali <neal.bhanushali@gmail.com>
2019-11-03 01:13:18 +0530

    get input and change for text and secret values

--- src/Commands/ChangeEnvValues.php
+++ src/Commands/ChangeEnvValues.php
@@ -3,6 +3,7 @@
 namespace NeelBhanushali\LaravelChangeEnvValues\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
 
 class ChangeEnvValues extends Command
 {
@@ -38,5 +39,24 @@ class ChangeEnvValues extends Command
     public function handle()
     {
         $keys = $this->argument('keys');
+
+        $env_file_path = app()->environmentFile();
+
+        foreach ($keys as $key) {
+            $is_secret = Str::startsWith($key, '.') ? 1 : 0;
+            $key = str_replace('.', '', $key);
+            $value = env($key);
+
+            $key_exploded = array_filter(explode('=', $key));
+            $is_value_provided = count($key_exploded) > 1;
+
+            $new_value = $is_value_provided ? $key_exploded[1] : ($is_secret ? $this->secret("Enter {$key}") : $this->ask("Enter {$key}", $value));
+
+            file_put_contents($env_file_path, str_replace(
+                "{$key}={$value}",
+                "{$key}={$new_value}",
+                file_get_contents($env_file_path)
+            ));
+        }
     }
 }
